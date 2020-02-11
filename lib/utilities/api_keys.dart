eaabe4d9b86dea7cd4fc498baef893a296f93d98
GroovinChip <groovinchip@gmail.com>
2020-02-11 10:10:22 -0500

    re-track keys file

---
+++ lib/utilities/api_keys.dart
@@ -0,0 +1,5 @@
+class ApiKeys {
+  static final String sentryDsn = 'https://9dfb88b691594bfbbbf1bf5fe33a751a@sentry.io/2043262';
+  static final String snapfeedProjectId = 'solosocial-6c3e1ze';
+  static final String snapfeedSecret = 'pl8pmylfgcjgla9fkib7c99jku';
+}
