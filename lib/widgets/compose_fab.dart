8d9e5aa223bc98336afa9a77f69d23aca364c9df
Reuben Turner <groovinchip@gmail.com>
2021-04-13 21:37:29 -0400

    Delete unused constructor

--- lib/widgets/compose_fab.dart
+++ lib/widgets/compose_fab.dart
@@ -1,10 +1,6 @@
 import 'package:solo_social/library.dart';
 
 class ComposeFab extends StatelessWidget {
-  const ComposeFab({
-    Key? key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Semantics(
